5834c4415284ad809c43f4abf0f0e3b85d8d6626
ribhu14engineer <ribhu14.engineer@gmail.com>
2023-10-31 06:30:44 +0530

    AccessModifiers and Constructors

--- src/accessModifier/Client.java
+++
@@ -1,9 +0,0 @@
-package accessModifier;
-
-public class Client {
-    public static void main(String[] args) {
-        Box.printMe();
-        Box b1 = new Box();
-        System.out.println(b1.height);
-    }
-}
